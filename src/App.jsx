c621164166ef99f357bb9e481a4e975e2ffca3de
Jyotiranjan Patra <112277029+Jrpatra706@users.noreply.github.com>
2023-12-15 23:17:14 +0530

    done with theme Switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/themebtn/ThemeBtn';
+import Card from './components/card/Card';
 
 function App() {
+  const [themeMode,setThemeMode] = useState('light');
+
+  const darkTheme = () =>{
+    setThemeMode('dark');
+  }
+
+  const lightTheme = () =>{
+    setThemeMode('light');
+  }
+
+  // to make actual change in theme, have to add themeMode to the classList of html
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('light','dark');
+    document.querySelector('html').classList.add(themeMode);
+  },[themeMode]);
 
   return (
     // value={{themeMode, darkTheme , lightTheme}}
@@ -11,10 +29,12 @@ function App() {
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
               {/* theme btn */}
+              <ThemeBtn/>
           </div>
 
           <div className="w-full max-w-sm mx-auto">
               {/* card */}
+              <Card/>
           </div>
         </div>
       </div>
